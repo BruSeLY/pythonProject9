a61e7e847f2b6cbfcafb77a2906ff1f83b179aa1
Даниил <gremlinglav@gmail.com>
2023-07-19 20:03:06 +0900

    Initial commit

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ users = {}
 print("END")
 
 side = ""
-
+currentMatch = None
 
 class User:
     def __init__(self, uid, name):
@@ -85,7 +85,7 @@ class User:
             f.write(f'{uid};{name};{self.points}')
 
     def bet(self, msg, put_points, match):
-        global side
+        global side, currentMatch
         win_or_loss = False
         if users[msg.from_user.id].points < put_points:
             bot.send_message(msg.chat.id, f"Недостаточно очков. Баланс: {users[msg.from_user.id].points}", reply_markup=types.ReplyKeyboardRemove())
@@ -99,17 +99,20 @@ class User:
                     win_or_loss = True
                 elif side == 1 and match[3] == "False":
                     win_or_loss = True
-                print(f"{win_or_loss}:: {side}:: {type(side)}:: {type(match[3])}")
-                if not(win_or_loss):
-                    users[msg.from_user.id].points -= put_points
-                    f.write(f'{msg.from_user.id};{self.points};{put_points};{side}{win_or_loss}')
-                    bot.send_message(msg.chat.id, f"@{self.name}, вы поставили {put_points} и проиграли. Баланс: {users[msg.from_user.id].points}", reply_markup=types.ReplyKeyboardRemove())
-                else:
-                    users[msg.from_user.id].points += (float(match[-1]) * put_points)
-                    f.write(f'{msg.from_user.id};{self.points};{put_points};{side}{win_or_loss}')
-                    bot.send_message(msg.chat.id, f"@{self.name}, вы поставили {put_points} и победили. Баланс: {users[msg.from_user.id].points}", reply_markup=types.ReplyKeyboardRemove())
+                with open("matches.txt", "a") as a:
+                    a.split("\n")
+
+                    if not(win_or_loss):
+                        users[msg.from_user.id].points -= put_points
+                        f.write(f'{msg.from_user.id};{self.points};{put_points};{side}{win_or_loss}')
+                        bot.send_message(msg.chat.id, f"@{self.name}, вы поставили {put_points} и проиграли. Баланс: {users[msg.from_user.id].points}", reply_markup=types.ReplyKeyboardRemove())
+                    else:
+                        users[msg.from_user.id].points += (float(match[-1]) * put_points)
+                        f.write(f'{msg.from_user.id};{self.points};{put_points};{side}{win_or_loss}')
+                        bot.send_message(msg.chat.id, f"@{self.name}, вы поставили {put_points} и победили. Баланс: {users[msg.from_user.id].points}", reply_markup=types.ReplyKeyboardRemove())
                 bot.send_message(msg.chat.id, f"@{self.name}, спасибо, что играете в нашу игру!!!")
         side = None
+        currentMatch = None
         win_or_loss = False
 
     def get_status(self):
@@ -117,12 +120,20 @@ class User:
 
 
 match = ""
+matchesDict = {}
+with open("matches.txt", "r") as f:
+    f = f.read()
+    f = f.split("\n")
+    for row in f:
+        row.split(";")
+        print(row)
 
+
+print(matchesDict)
 
 @bot.message_handler(content_types=["text"])
 def main_text_logic(msg):
-    global match
-    global users
+    global match, currentMatch, users
     if msg.from_user.id not in users:
         users[msg.from_user.id] = User(msg.from_user.id, msg.from_user.first_name)
     print(users[msg.from_user.id].user_id, users[msg.from_user.id].points, users[msg.from_user.id].status)
@@ -141,6 +152,7 @@ def main_text_logic(msg):
                 match = f[0].split(';')
 
                 print(match)
+                currentMatch = match[0]
                 direThis1 = match[2][1:-1].split(", ")
                 direThis = [i[1:-1] for i in direThis1]
                 radiantThis1 = match[1][1:-1].split(", ")
@@ -151,7 +163,7 @@ def main_text_logic(msg):
                 btn1 = types.KeyboardButton("bet Radiant")
                 btn2 = types.KeyboardButton("bet Dire")
                 btn3 = types.KeyboardButton("Вернуться назад!")
-                markup.add(btn1, btn2, btn3)
+                markup.add(btn1, btn3, btn2)
 
                 bot.send_message(msg.chat.id, f'@{msg.from_user.first_name} \nВыберите команду, которая победит\nЧтобы это сделать введите <bet Radiant> или <bet Dire> без ковычек', reply_markup=markup)
                 users[msg.from_user.id].status = "vote"
